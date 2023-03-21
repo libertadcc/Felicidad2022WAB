9f51b95abb81912752c168ec21dfdf4aced28580
Libertad Chapinal Cervantes <libertad.chapinal@esri.es>
2023-03-21 19:39:23 +0100

    ADD felicidad app

---
+++ widgets/FiltroFelicidad/Widget.js
@@ -0,0 +1,80 @@
+define(['dojo/_base/declare', 'jimu/BaseWidget'],
+  function(declare, BaseWidget) {
+    //To create a widget, you need to derive from BaseWidget.
+    return declare([BaseWidget], {
+      // Custom widget code goes here
+
+      baseClass: 'jimu-widget-customwidget',
+
+      postCreate:function() {
+        console.log(this.map);
+        layer = this.map.getLayer('Índice_Felicidad_Mundial_920');
+      },
+      
+      filtrarPais: function() {
+        var valorUser = this.inputPais.value;
+        layer.setDefinitionExpression("Country = '"+ valorUser +"'")
+        //`Country = '${valorusuario}'`
+      },
+      filtrarRanking: function(){
+        var valorUser = this.inputRank.value;
+        layer.setDefinitionExpression("Rank = '"+ valorUser +"'")
+      },
+      restaurar() {
+        layer.setDefinitionExpression(layer.defaultDefinitionExpression)
+      }
+
+      //this property is set by the framework when widget is loaded.
+      //name: 'CustomWidget',
+
+
+      //methods to communication with app container:
+
+      // postCreate: function() {
+      //   this.inherited(arguments);
+      //   console.log('postCreate');
+      // },
+
+      // startup: function() {
+      //  this.inherited(arguments);
+      //  this.mapIdNode.innerHTML = 'map id:' + this.map.id;
+      //  console.log('startup');
+      // },
+
+      // onOpen: function(){
+      //   console.log('onOpen');
+      // },
+
+      // onClose: function(){
+      //   console.log('onClose');
+      // },
+
+      // onMinimize: function(){
+      //   console.log('onMinimize');
+      // },
+
+      // onMaximize: function(){
+      //   console.log('onMaximize');
+      // },
+
+      // onSignIn: function(credential){
+      //   /* jshint unused:false*/
+      //   console.log('onSignIn');
+      // },
+
+      // onSignOut: function(){
+      //   console.log('onSignOut');
+      // }
+
+      // onPositionChange: function(){
+      //   console.log('onPositionChange');
+      // },
+
+      // resize: function(){
+      //   console.log('resize');
+      // }
+
+      //methods to communication between widgets:
+
+    });
+  });
